sbt/docker-sbt

commit d27f4a6a396304f37ac45001f3a8ea87cd1a2fcf

Run as non-root (#63)
- running docker as root can be problematic (one has root privileges on
 the host)
 - running docker as root is disallow in some environments
 - this adds a users sbtuser (to keep it separate from the deamon user
 sbt)
 - and switches to the user sbtuser
 - it changes the CMD to sbt

Dockerfile
@@ -33,5 +33,14 @@ RUN \
   sbt compile && \
   rm -r project && rm build.sbt && rm Temp.scala && rm -r target
 
+# Add and use user sbt
+RUN groupadd --gid 1001 sbtuser && useradd --gid 1001 --uid 1001 sbtuser --shell /bin/bash
+RUN chown -R sbtuser:sbtuser /opt
+RUN mkdir /home/sbtuser && chown -R sbtuser:sbtuser /home/sbtuser
+RUN mkdir /logs && chown -R sbtuser:sbtuser /logs
+USER sbtuser
+
 # Define working directory
-WORKDIR /root
+WORKDIR /home/sbtuser
+
+CMD ["sbt"]

commit 05a2dda0e70eec5ec45b27c1a32b1f1872b7267f

Fixing regressions introduced with PR #63  - reorganize Dockerfile  - download and install sbt as root  - create and switch to sbtuser  - download and provide Scala  - prepare sbt  - fixes #64

Dockerfile
@@ -7,16 +7,10 @@
 # Pull base image
 FROM openjdk:11.0.2
 
+
 # Env variables
 ENV SCALA_VERSION 2.12.8
 ENV SBT_VERSION 1.2.8
-
-# Install Scala
-## Piping curl directly in tar
-RUN \
-  curl -fsL https://downloads.typesafe.com/scala/$SCALA_VERSION/scala-$SCALA_VERSION.tgz | tar xfz - -C /root/ && \
-  echo >> /root/.bashrc && \
-  echo "export PATH=~/scala-$SCALA_VERSION/bin:$PATH" >> /root/.bashrc
 
 # Install sbt
 RUN \
@@ -24,16 +18,9 @@ RUN \
   dpkg -i sbt-$SBT_VERSION.deb && \
   rm sbt-$SBT_VERSION.deb && \
   apt-get update && \
-  apt-get install sbt && \
-  sbt sbtVersion && \
-  mkdir project && \
-  echo "scalaVersion := \"${SCALA_VERSION}\"" > build.sbt && \
-  echo "sbt.version=${SBT_VERSION}" > project/build.properties && \
-  echo "case object Temp" > Temp.scala && \
-  sbt compile && \
-  rm -r project && rm build.sbt && rm Temp.scala && rm -r target
+  apt-get install sbt
 
-# Add and use user sbt
+# Add and use user sbtuser
 RUN groupadd --gid 1001 sbtuser && useradd --gid 1001 --uid 1001 sbtuser --shell /bin/bash
 RUN chown -R sbtuser:sbtuser /opt
 RUN mkdir /home/sbtuser && chown -R sbtuser:sbtuser /home/sbtuser
@@ -42,5 +29,22 @@ USER sbtuser
 
 # Define working directory
 WORKDIR /home/sbtuser
+
+# Install Scala
+## Piping curl directly in tar
+RUN \
+  curl -fsL https://downloads.typesafe.com/scala/$SCALA_VERSION/scala-$SCALA_VERSION.tgz | tar xfz - -C /home/sbtuser/ && \
+  echo >> /home/sbtuser/.bashrc && \
+  echo "export PATH=~/scala-$SCALA_VERSION/bin:$PATH" >> /home/sbtuser/.bashrc
+
+# Prepare sbt
+RUN \
+  sbt sbtVersion && \
+  mkdir -p project && \
+  echo "scalaVersion := \"${SCALA_VERSION}\"" > build.sbt && \
+  echo "sbt.version=${SBT_VERSION}" > project/build.properties && \
+  echo "case object Temp" > Temp.scala && \
+  sbt compile && \
+  rm -r project && rm build.sbt && rm Temp.scala && rm -r target
 
 CMD ["sbt"]